6a6a100078221653fa9b46dbfad90f58dbd6017c
seohan1010 <seohan1010@hotmail.com>
2023-09-20 01:14:58 +0900

    ch8_4 initial commit

---
+++ src/test/java/com/seohan1010/ch8_4/Ch84ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seohan1010.ch8_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch84ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
