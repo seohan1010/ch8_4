50e95af731f7629f2d7609eabfb1d7ec697f9400
seohan1010 <seohan1010@hotmail.com>
2023-10-31 23:53:23 +0900

    file upload testing

--- src/main/java/com/seohan1010/ch8_4/controller/BoardController.java
+++ src/main/java/com/seohan1010/ch8_4/controller/BoardController.java
@@ -7,6 +7,7 @@ import com.seohan1010.ch8_4.to.BoardDto;
 import com.seohan1010.ch8_4.to.PageHandler;
 import com.seohan1010.ch8_4.to.SearchCondition;
 import org.apache.catalina.webresources.FileResource;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
@@ -16,15 +17,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystem;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @CrossOrigin(origins = {"http://localhost:3000", "*"})
 @RestController
@@ -41,44 +40,52 @@ public class BoardController {
     @Value("${uploadPath}")
     private String uploadPath;
 
-    @RequestMapping(value="/uploadFile",method=RequestMethod.POST)
-    public ResponseEntity<HttpStatus> uploadFile(MultipartFile[] files){
 
-        System.out.println("files = " + files);
-       try{
-           for(MultipartFile file: files){
-           System.out.println("<<<<<<<<<<<<<< >>>>>>>>>>>>>>>>>");
-               System.out.println("file.getOriginalFilename() = " + file.getOriginalFilename());
-               System.out.println("file.getSize() = " + file.getSize());
-               File upFile = new File(uploadPath,file.getOriginalFilename());
-               file.transferTo(upFile);
-           }
-       }catch(Exception e){
-           System.out.println(e.getMessage());
-           e.printStackTrace();
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public ResponseEntity<HttpStatus> uploadFile(MultipartHttpServletRequest request) {
+        System.out.println("request.getRequestHeaders() = " + request.getRequestHeaders());
+        System.out.println("request.getFileNames() = " + request.getFileNames());
+        System.out.println("request.getHttpServletMapping() = " + request.getHttpServletMapping());
+
+        try {
+
+//            System.out.println("files = " + files);
+//
+//
+//            for (MultipartFile file : files) {
+//                System.out.println("<<<<<<<<<<<<<< >>>>>>>>>>>>>>>>>");
+//                System.out.println("file.getOriginalFilename() = " + file.getOriginalFilename());
+//                System.out.println("file.getSize() = " + file.getSize());
+//                File upFile = new File(uploadPath, file.getOriginalFilename());
+//                file.transferTo(upFile);
+//            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
             return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
-       }
+
+        }
 
         return new ResponseEntity<HttpStatus>(HttpStatus.OK);
 
     }
 
 
-    @RequestMapping(value="/download",produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity<FileSystemResource> downloadFile(String fileName){
+    @RequestMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<FileSystemResource> downloadFile(String fileName) {
         System.out.println("fileName = " + fileName);
 
-           FileSystemResource resource = new FileSystemResource(uploadPath+fileName);
+        FileSystemResource resource = new FileSystemResource(uploadPath + fileName);
 
-            return null;
+        return null;
 
     }
 
-    public ResponseEntity<String> deleteFile(String fileName){
+    public ResponseEntity<String> deleteFile(String fileName) {
 
         System.out.println("fileName = " + fileName);
-        File file = new File(uploadPath+fileName);
-        if(file.delete()==true){
+        File file = new File(uploadPath + fileName);
+        if (file.delete() == true) {
             return new ResponseEntity<>(HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -94,13 +101,12 @@ public class BoardController {
     }
 
 
-
     // 프론트에서 유효하지 못한 값들이 넘어오면 에러 코드와 함께 빈배열을 반환한다.
     // 페이지 핸들러로 검색된 게시판의 totalCnt값을 넘겨줘서 계산한 값들을 백단에서 같이 넘겨준다. 검색한 후에는 검색된 결과에 대한
     // 페이징이 가능하게 하는 프론트 단의 로직이 필요하다.
     // ---> map 으로 넘겨주어야 할거 같다(검색된 게시판의 pageHandler 결과를 넘겨줘서 페이징을 하기 위해서 이다. ).
     @RequestMapping(value = "/search", method = RequestMethod.POST)
-    public ResponseEntity<Map<String,Object>> searchBoardList(@RequestBody SearchCondition sc) throws Exception {
+    public ResponseEntity<Map<String, Object>> searchBoardList(@RequestBody SearchCondition sc) throws Exception {
         List<BoardDto> list = null;
         System.out.println("<<<<<<<<<<<<<<<< sc = " + sc);
 
@@ -112,8 +118,6 @@ public class BoardController {
         if (pageSize == null) pageSize = 10;
 
 
-
-
         SearchCondition sc2 = new SearchCondition();
         sc2.setPage(page);
         sc2.setPageSize(pageSize);
@@ -122,19 +126,19 @@ public class BoardController {
         int totalCnt = boardService.searchBoardListCnt(sc);
         PageHandler ph = new PageHandler(totalCnt, sc);
 
-        Map<String,Object> map=null;
+        Map<String, Object> map = null;
         try {
             list = boardService.searchBoardList(sc);
-            map= new HashMap<>();
-            map.put("ph",ph);
-            map.put("list",list);
+            map = new HashMap<>();
+            map.put("ph", ph);
+            map.put("list", list);
             System.out.println(list == null ? ">>>>>>>>>>>>>no data found" : list);
             if (list.size() == 0)
-                return new ResponseEntity<Map<String,Object>>(map, HttpStatus.BAD_REQUEST); // 204번 에러 코드
-            return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK); // 200번 코드
+                return new ResponseEntity<Map<String, Object>>(map, HttpStatus.BAD_REQUEST); // 204번 에러 코드
+            return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK); // 200번 코드
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<Map<String,Object>>(map, HttpStatus.BAD_REQUEST); // 400번 코드
+            return new ResponseEntity<Map<String, Object>>(map, HttpStatus.BAD_REQUEST); // 400번 코드
         }
 
     }
@@ -168,7 +172,7 @@ public class BoardController {
             // 전체 게시물의 개수를 가지고 오고
             int totalCnt = boardService.getCount();
             //
-            PageHandler pageHandler = new PageHandler(totalCnt, new SearchCondition("","",page,pageSize));
+            PageHandler pageHandler = new PageHandler(totalCnt, new SearchCondition("", "", page, pageSize));
 
             // 해당 범위에 있는 게시물을 가져오기위함
             map.put("offset", (page - 1) * pageSize);
@@ -191,7 +195,7 @@ public class BoardController {
             e.printStackTrace();
             map.clear();
             // 에러가 발생했을때에도 값을 넣어주어야 프론트단에서 에러 핸들링이 쉬워진다.
-            map.put("list",list);
+            map.put("list", list);
             return new ResponseEntity<Map<String, Object>>(map, HttpStatus.BAD_REQUEST); // 400번 코드를 반환
         }
 
