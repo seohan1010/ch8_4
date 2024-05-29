2286094157d8218323c8663255dd2736d52a3dd9
seohan1010 <seohan1010@hotmail.com>
2024-05-29 22:10:50 +0900

    cors related configuration modified

--- src/main/java/com/seohan1010/ch8_4/common/WebConfig.java
+++ src/main/java/com/seohan1010/ch8_4/common/WebConfig.java
@@ -1,7 +1,13 @@
 package com.seohan1010.ch8_4.common;
 
 
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,9 +17,32 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**").allowedMethods("*")
-                .allowedOrigins("*");
+                .allowedOrigins("*").allowedHeaders("*");
 
     }
+
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean(){
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",config);
+
+        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));
+        filterBean.setOrder(0);
+        return filterBean;
+
+
+
+    }
+
 
 
 }
