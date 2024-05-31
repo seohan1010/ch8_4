d18de79926685c1c6beae5baa3b998b73e3e1c3b
seohan1010 <seohan1010@hotmail.com>
2024-05-31 23:51:49 +0900

    application.properties modified

--- src/main/java/com/seohan1010/ch8_4/common/WebConfig.java
+++ src/main/java/com/seohan1010/ch8_4/common/WebConfig.java
@@ -17,9 +17,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**").allowedMethods("*")
-                .allowedOrigins("*").allowedHeaders("*");
+                .allowedOrigins("*").allowedHeaders("*").allowedOriginPatterns("*").exposedHeaders("*");
+
 
     }
+
+
+
 
 
 //    @Bean
