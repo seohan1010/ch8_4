b889dd69421202835571da8e52ae0f10a103f60e
seohan1010 <seohan1010@hotmail.com>
2025-10-08 16:35:59 +0900

    Update BoardCommentController.java

--- src/main/java/com/seohan1010/ch8_4/controller/BoardCommentController.java
+++ src/main/java/com/seohan1010/ch8_4/controller/BoardCommentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3001", "*"},
+@CrossOrigin(origins = {"http://localhost:3000", "*"},
                         methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 @RequestMapping(value = "/boardcomment")
 public class BoardCommentController {
