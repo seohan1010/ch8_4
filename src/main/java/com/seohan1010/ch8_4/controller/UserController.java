b395cb4a9243dd9dcf94612e350debafbae80f60
seohan1010 <seohan1010@hotmail.com>
2025-10-08 16:33:31 +0900

    Update UserController.java

--- src/main/java/com/seohan1010/ch8_4/controller/UserController.java
+++ src/main/java/com/seohan1010/ch8_4/controller/UserController.java
@@ -20,7 +20,8 @@ import java.util.Map;
 
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3001", "*"})
+@CrossOrigin(origins = {"http://localhost:3001", "*"},
+             methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 @RequestMapping("/user")
 public class UserController {
 
