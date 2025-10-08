1904f57228652905517550b0581051c14cc38613
seohan1010 <seohan1010@hotmail.com>
2025-10-08 16:33:03 +0900

    Update MainController.java

--- src/main/java/com/seohan1010/ch8_4/controller/MainController.java
+++ src/main/java/com/seohan1010/ch8_4/controller/MainController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3001", "*"})
+@CrossOrigin(origins = {"http://localhost:3001", "*"},
+             methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 public class MainController {
 
 
