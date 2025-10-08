401ff82e044336e500911676db48fc3c99bf07c9
seohan1010 <seohan1010@hotmail.com>
2025-10-08 17:51:46 +0900

    Update BoardCommentController.java

--- src/main/java/com/seohan1010/ch8_4/controller/BoardCommentController.java
+++ src/main/java/com/seohan1010/ch8_4/controller/BoardCommentController.java
@@ -12,7 +12,8 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000",
-                        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
+             methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS},
+             allowCredentials = false )
 @RequestMapping(value = "/boardcomment")
 public class BoardCommentController {
 
