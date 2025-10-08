3bf1527c741a8ee5a24f00164ca083ed3f47fba4
seohan1010 <seohan1010@hotmail.com>
2025-10-08 18:02:06 +0900

    Update BoardController.java

--- src/main/java/com/seohan1010/ch8_4/controller/BoardController.java
+++ src/main/java/com/seohan1010/ch8_4/controller/BoardController.java
@@ -28,7 +28,7 @@ import java.util.*;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000",
              methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS},
-             allowCredentials = false )
+             allowCredentials = "false" )
 @RequestMapping("/board")
 public class BoardController {
 
